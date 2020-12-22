43dcd5928c30ee15d604829257622f29d9659050
ericTle1 <57995708+ericTle1@users.noreply.github.com>
2020-12-22 16:01:57 -0500

    Update install_sqlite_tables.sql

--- src/install_sqlite_tables.sql
+++ src/install_sqlite_tables.sql
@@ -5,10 +5,3 @@ INSERT INTO 'results' (goodsType, searchNum) select  "cheese", 6 WHERE NOT EXIST
 INSERT INTO 'results' (goodsType, searchNum) select  "eggs", 15 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 3);
 INSERT INTO 'results' (goodsType, searchNum) select  "seafood", 4 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 4);
 INSERT INTO 'results' (goodsType, searchNum) select  "herbs", 13 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 5);
-INSERT INTO 'results' (goodsType, searchNum) select  "honey", 13 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 6);
-INSERT INTO 'results' (goodsType, searchNum) select  "jams", 14 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 7);
-INSERT INTO 'results' (goodsType, searchNum) select  "vegetables", 17 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 8);
-INSERT INTO 'results' (goodsType, searchNum) select  "maple", 2 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 9);
-INSERT INTO 'results' (goodsType, searchNum) select  "meat", 7 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 10);
-INSERT INTO 'results' (goodsType, searchNum) select  "nuts", 4 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 11);
-INSERT INTO 'results' (goodsType, searchNum) select  "wine", 6 WHERE NOT EXISTS (SELECT * FROM 'results' WHERE id = 12);
